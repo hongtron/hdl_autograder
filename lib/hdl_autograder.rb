7a7697c5265d9a63b51a5d9e06dfa2f0235d003e
ali <github@alihong.net>
2019-01-13 15:41:39 -0600

    move all grading configs to yaml

--- lib/hdl_autograder.rb
+++ lib/hdl_autograder.rb
@@ -7,7 +7,6 @@ require 'hdl_autograder/chip'
 require 'hdl_autograder/project'
 require 'hdl_autograder/implementation'
 require 'hdl_autograder/grader'
-require 'hdl_autograder/rubrics'
 require 'hdl_autograder/simulator'
 require 'hdl_autograder/submission'
 
