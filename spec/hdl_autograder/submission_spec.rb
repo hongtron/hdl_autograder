c0cd86758daefe39b096a0d1ae197843ba1f7044
ali <github@alihong.net>
2019-01-14 21:23:45 -0600

    add spec for #hdl_files

--- spec/hdl_autograder/submission_spec.rb
+++ spec/hdl_autograder/submission_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe HdlAutograder::Submission do
+  describe "#hdl_files" do
+    it "ignore builtin hdl files" do
+      project = HdlAutograder::Project.new(2)
+      submission = HdlAutograder::Submission.new(project, "./bin/nand2tetris_tools/builtInChips")
+      expect(submission.hdl_files.size).to eq(0)
+    end
+  end
 end
